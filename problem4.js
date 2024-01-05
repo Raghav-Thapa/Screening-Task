ae0ced8d00e35ed799d025877cc2175e6f86d28d
Raghav Thapa <raghavthapa7@gmail.com>
2024-01-05 13:53:46 +0545

    problem 4 updated

--- problem4.js
+++ problem4.js
@@ -1,10 +1,19 @@
 function boxy(n) {
-    let result = ' '
+    let result = ''
+    let print = ''
     for(i = 1; i <= n; i++){
         result += `|${i}|`
+        print += ' - '
     }
+    console.log(print)
     console.log(result)
+    console.log(print)
 }
 
-boxy(1)
+// boxy(1)
+
 boxy(3)
+
+
+
+
